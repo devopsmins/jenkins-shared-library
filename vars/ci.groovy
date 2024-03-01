22c386b636fe58a4fe163d0f99ebf021408abd54
minhaj <minhajfathima1023@gmail.com>
2024-03-01 12:31:09 -0500

    Prometheus ansible change

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,14 +1,19 @@
 def call(){
     node ('workstation' ) {
+      sh 'env'
+        if(BRANCH_NAME == "main")
+        {
+            stage( 'Code Checkout' ) {}
+            stage( 'Compile' ) {}
+            stage( 'Build' ) {}
+        }
+        //stage( 'Compile' ) {}
 
+        //stage( 'Test Case' ) {}
 
-        stage( 'Compile' ) {}
+        //stage( 'Build' ) {}
 
-        stage( 'Test Case' ) {}
-
-        stage( 'Build' ) {}
-
-        stage( 'Release App' ) {}
+        //stage( 'Release App' ) {}
 
     }
 
