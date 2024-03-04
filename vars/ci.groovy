ac41649c3b44250c186e182926939b08475a5695
minhaj <minhajfathima1023@gmail.com>
2024-03-04 03:03:56 -0500

    Prometheus ansible change

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -4,7 +4,7 @@ def call(){
         sh "find . | sed -e '1d' |xargs rm -rf"
 
         if(env.TAG_NAME ==~ ".*") {
-            env.branch_name == "refs/tags/${env.TAG_NAME}"
+            env.branch_name == "${env.TAG_NAME}"
         } else {
             env.branch_name == "${env.BRANCH_NAME}"
         }
@@ -14,7 +14,7 @@ def call(){
                     branches: [[name: "${branch_name}"]],
                     userRemoteConfigs: [[url: "https://github.com/devopsmins/expense-backend.git"]]
            )
-            sh 'cat jenkinsfile'
+            sh 'cat Jenkinsfile'
         }
 
 
