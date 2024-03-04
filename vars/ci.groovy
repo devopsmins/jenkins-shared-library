6825eff4818d0e4b2851e39c40c28570a29fbafb
minhaj <minhajfathima1023@gmail.com>
2024-03-04 10:14:55 -0500

    TF code

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -17,7 +17,7 @@ def call(){
             //git branch: 'main', url: 'https://github.com/expenseapp-v1/expense-backend.git'
            checkout scmGit(
                     branches: [[name: "${branch_name}"]],
-                    userRemoteConfigs: [[url: "https://github.com/devopsmins/expense-backend.git"]]
+                    userRemoteConfigs: [[url: "https://github.com/devopsmins/expense-backend"]]
            )
             sh 'cat Jenkinsfile'
         }
