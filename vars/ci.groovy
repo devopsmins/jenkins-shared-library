d26f03e3b058e4e447e7ed05e2a3c9e19e1034a1
minhaj <minhajfathima1023@gmail.com>
2024-03-04 13:13:37 -0500

    TF code

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -4,7 +4,7 @@ def call(){
         sh "find . | sed -e '1d' |xargs rm -rf"
 
         if(env.TAG_NAME ==~ ".*") {
-            env.branch_name == "${env.TAG_NAME}"
+            env.branch_name == "refs/tags/${env.TAG_NAME}"
         } else {
             if(env.BRANCH_NAME ==~ "PR-.*") {
                 env.branch_name == "${env.CHANGE_BRANCH}"
