54b9d9c40a64e85045c50831a631e99922282996
minhaj <minhajfathima1023@gmail.com>
2024-03-05 03:50:15 -0500

    TF code

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -13,11 +13,11 @@ def call(){
             }
         }
         stage( 'Code Checkout' ) {
-            git branch: "${env.branch_name}", url: 'https://github.com/devopsmins/expense-backend.git'
-           //checkout scmGit(
-                    //branches: [[name: "${branch_name}"]],
-                    //userRemoteConfigs: [[url: "https://github.com/devopsmins/expense-backend"]]
-          // )
+            //git branch: "${env.branch_name}", url: 'https://github.com/devopsmins/expense-backend.git'
+           checkout scmGit(
+                    branches: [[name: "${branch_name}"]],
+                    userRemoteConfigs: [[url: "https://github.com/devopsmins/expense-backend"]]
+           )
             sh 'cat Jenkinsfile'
         }
 
