bd5bd9a57043e59c367e13d0fbf36c96ebaa36b8
minhaj <minhajfathima1023@gmail.com>
2024-03-05 02:02:42 -0500

    TF code

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -5,13 +5,10 @@ def call(){
 
         if(env.TAG_NAME ==~ ".*") {
             env.branch_name == "refs/tags/${env.TAG_NAME}"
-        } else {
-            if(env.BRANCH_NAME ==~ "PR-.*") {
-                env.branch_name == "${env.CHANGE_BRANCH}"
-            } else {
+        }  else {
             env.branch_name == "${env.BRANCH_NAME}"
             }
-        }
+
         sh 'env'
         stage( 'Code Checkout' ) {
             //git branch: 'main', url: 'https://github.com/expenseapp-v1/expense-backend.git'
