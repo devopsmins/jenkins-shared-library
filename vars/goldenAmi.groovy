804a4e4fef434c945f85a9c0f4109044c36a2223
minhaj <minhajfathima1023@gmail.com>
2024-03-15 05:25:14 -0400

    TF code

--- vars/goldenAmi.groovy
+++ vars/goldenAmi.groovy
@@ -1,6 +1,11 @@
 def call() {
+    ansicolor ('xterm') {
     node ('workstation' ) {
         common.codeCheckout()
-
+        stage ('Terrafrom Apply') {
+            sh 'terraform init'
+            sh 'terraform apply -auto-approve '
+        }
+    }
     }
 }
