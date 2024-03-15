bbdfa9f97a8551cde981b12b9e566b0a8bf78664
minhaj <minhajfathima1023@gmail.com>
2024-03-15 06:35:59 -0400

    TF code

--- vars/goldenAmi.groovy
+++ vars/goldenAmi.groovy
@@ -1,12 +1,12 @@
 def call() {
     ansiColor ('xterm') {
-    node ('workstation' ) {
+        node ('workstation' ) {
         common.codeCheckout()
-        stage ('Terraform Apply') {
+        stage('Terraform Apply') {
             sh 'terraform init'
             //sh 'terraform apply -auto-approve '
         }
-        stage ('Terraform Destroy') {
+        stage('Terraform Destroy') {
             sh 'terraform init'
             sh 'terraform state rm aws_ami_from_instance.ami'
             sh 'terraform destroy -auto-approve '
